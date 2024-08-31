cb48ecca9c7371299d1449a5a045b42c926e93b9
syebil <168447494+syebil@users.noreply.github.com>
2024-08-31 19:42:29 +0700

    Add files via upload

---
+++ Activitas.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Activity</title>
+</head>
+
+<style>
+
+.bebek {
+            background-color: black; 
+            text-decoration:none;
+            text-align:center;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin: 10px;
+            padding: 10px;
+            flex: 1;
+        }
+
+    body {
+  background: linear-gradient(to right, #EECAD5, #D1E9F6 );
+  font-family: 'Trebuchet MS';
+  color: black;
+  margin-right: ;
+  text-align:center;
+
+  img{
+    width: 20%;
+    height: 30;
+  }
+}
+</style>
+<body>
+    <h1>Hallo</h1>
+    <h3><p>perkenalkan nama saya Muhammad Sabil
+        saya memulai aktivitas saya dengan melakukan solat subuh di lanjut dengan sarapan atau langsung mandi<br>
+        setelah mandi biasa saya manasin motor dilanjut berangkat ke sekolah sekitar jam 06:00 pagi<br>
+        di sekolah saya mengikuti pelajaran aja sih mau ngapain lagi?<br>
+        biasanya kalo pulang di atas jam 5 saya solat sebentar abis itu tiduran aja sih<br>
+        kalo di bawah jam 5 saya mandi sekalian nunngu solat<br>
+        setelah solat sih saya biasa nya makan kalo enggak main hp sampe jam 11an<br>
+    </h3></p>
+    <img src="zeniki.jpg">
+    <br>
+    <br>s
+<a href="home.php" class="bebek">Kembali Ke Home</a>
+</body>
+</html>
